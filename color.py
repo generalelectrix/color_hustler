7b47647103592bce3eed50a9d23ff9d182d2cf23
Chris Macklin <generalelectrix@gmail.com>
2015-09-07 04:02:46 +0100

    ENH: Add ColorSwarm class.

--- color.py
+++ color.py
@@ -1,3 +1,5 @@
+from random import Random
+
 # color organ style HSB color
 
 class Color(object):
@@ -72,3 +74,41 @@ class HSBColorGenerator(object):
         s_val = self.s_gen.get_constrained(0.0, 1.0)
         b_val = self.b_gen.get_constrained(0.0, 1.0)
         return Color(h_val, s_val, b_val)
+
+class ColorSwarm(object):
+    """Agglomerate multiple color generators."""
+    def __init__(self, col_gens, random=False, seed=None):
+        """Create a new ColorSwarm.
+
+        Args:
+            col_gens: ([color generator]) A list of color generators to agglomerate.
+            random (default=False): pick a generator randomly when get is called.
+            seed (optional): a seed for the swarm's internal RNG
+        """
+        self.random = random
+        self.rand_gen = Random()
+        if seed is not None:
+            self.rand_gen.seed(seed)
+        self.gens = col_gens
+        self.next = 0
+
+    def get(self):
+        """Get a color from the next generator."""
+        if self.random:
+            gen = self.rand_gen.randint(0, len(self.gens)-1)
+            return self.gens[gen].get()
+        else:
+            # advance to the next generator first in case the number of gens has
+            # changed since the last call.
+            self.next = (self.next + 1) % len(self.gens)
+            col = self.gens[self.next].get()
+            return col
+
+
+
+
+
+
+
+
+
