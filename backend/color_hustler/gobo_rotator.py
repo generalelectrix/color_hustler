a2ab550fea9277f4d7b1aa1125fdd0be5c503635
general electrix <general.electrix@gmail.com>
2019-09-09 23:03:39 -0700

    Add measurements for Smart Move DMX.

--- backend/color_hustler/gobo_rotator.py
+++ backend/color_hustler/gobo_rotator.py
@@ -1,7 +1,6 @@
 """Fixture driver for various gobo rotators.
 
-Reverse-engineered profiles:
-Roto-Q DMX:
+--- Roto-Q DMX ---
 0: stopped
 
 Max speed: DMX value 1, about 0.43 rot/sec
@@ -53,6 +52,46 @@ Some measurements:
     (147, 0.0169),
     (137, 0.01),
     (128, 0.00479),
+]
+
+--- Smart Move DMX ---
+
+Some odd bucketing:
+5 6 7 8 are the same speed
+18 19 same speed
+
+124 is slowest
+133 is slowest in other direction
+125-132 is stopped
+
+251 250 249 248 same speed
+238 237 same speed
+
+Measurements:
+[
+    (255, 0.344),
+    (249, 0.293),
+    (245, 0.27),
+    (235, 0.223),
+    (225, 0.191),
+    (215, 0.166),
+    (205, 0.141),
+    (195, 0.116),
+    (193, 0.107),
+    (191, 0.0972),
+    (189, 0.0909),
+    (187, 0.0794),
+    (185, 0.0604),
+    (184, 0.0481),
+    (183, 0.0406),
+    (181, 0.0306),
+    (179, 0.0244),
+    (176, 0.0194),
+    (175, 0.0175),
+    (165, 0.0102),
+    (155, 0.00583),
+    (145, 0.00270),
+    (135, 0.00193),
 ]
 """
 
