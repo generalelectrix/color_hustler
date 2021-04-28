81058cb7de4f0a164e3402196da2d46678cf112d
general electrix <general.electrix@gmail.com>
2021-04-27 22:20:33 -0700

    Update dependencies. Fix state bug in foldable.

--- frontend/src/Foldable.js
+++ frontend/src/Foldable.js
@@ -1,32 +1,28 @@
 // A component that folds details away.
-import React from 'react';
+import React from "react";
 import { FaCaretRight, FaCaretDown } from "react-icons/fa";
-import './Foldable.css'
+import "./Foldable.css";
 
-const Foldable = props => {
-
+const Foldable = (props) => {
   const [visible, setVisible] = React.useState(
-    props.startVisible === undefined ? true : props.startVisible)
+    props.startVisible === undefined ? true : props.startVisible
+  );
 
-  if (visible) {
-    return (
-      <div className="fold-container flexcol">
-        <div>
-          <FaCaretDown size={20} onClick={_ => setVisible(false)}/>
-          <span>{props.label}</span>
-        </div>
-        {props.children}
-      </div>
-    )
-  }
+  const caret = visible ? (
+    <FaCaretDown size={20} onClick={(_) => setVisible(false)} />
+  ) : (
+    <FaCaretRight size={20} onClick={(_) => setVisible(true)} />
+  );
 
   return (
     <div className="fold-container flexcol">
-      <FaCaretRight size={20} onClick={_ => setVisible(true)}/>
-      <span className="folded">{props.label}</span>
+      <div>
+        {caret}
+        <span className={visible ? null : "folded"}>{props.label}</span>
+      </div>
+      <div style={{ display: visible ? null : "none" }}>{props.children}</div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Foldable
+export default Foldable;
